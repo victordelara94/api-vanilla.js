b2d956c9e5783cab62a2b1542134b55c1420ea46
martiap <martiap93@gmail.com>
2023-10-27 21:14:25 +0200

    Create footer and add it to main.js

---
+++ components/footer.js
@@ -0,0 +1,38 @@
+import { Component } from "./component.js";
+
+export class Footer extends Component {
+  constructor(selector) {
+    super(selector);
+    this.template = this.createTemplate();
+    this.render();
+  }
+
+  createTemplate() {
+    return `
+      <footer>
+        <adress>
+          <div>
+            <span>By Marta</span>
+            <a href="https://github.com/martiap" target="_blank">
+              <img
+                src="../assets/github-logo.svg"
+                size="50"
+                alt="Logo gitHub"
+              ></img>
+            </a>
+          </div>
+          <div>
+            <span>By Victor</span>
+            <a href="https://github.com/victordelara94" target="_blank">
+              <img
+                src="../assets/github-logo.svg"
+                size="50"
+                alt="Logo gitHub"
+              ></img>
+            </a>
+          </div>
+        </adress>
+      </footer>
+    `;
+  }
+}
